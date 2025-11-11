d44be2bb5c317773d73eae144cf586d9a260cc0f
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-10 23:12:05 -0300

    Desenvolvimento do CRUD com Flask - Delete

--- app.py
+++ app.py
@@ -56,6 +56,18 @@ def update_task(id):
 
     return jsonify({"message": "Tarefa atualizada com sucesso!"})
 
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+
+    if not task:
+        return jsonify({"message", "Não foi possível encontrar a tarefa!"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
